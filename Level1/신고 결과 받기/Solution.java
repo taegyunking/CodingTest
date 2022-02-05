367b21db56e9d349d0d4417e2bd77ec60662d1be
tgan <atg940706@gmail.com>
2022-02-06 01:12:12 +0900

    신고 결과 받기 문제 업데이트(해시 사용)
    1. HashSet을 사용해서 report 내용의 중복을 제거
    2. user id list와 메일 받은 횟수를 저장하기 위한 idInfo를 만들고 전달받은 id_list와 동일하게 순서를 유지하기 위해 LinkedHashMap 사용
    3. 신고 당한 id와 신고 한 id list를 HashMap을 통해 정리

--- Level1/신고 결과 받기/Solution.java
+++ Level1/신고 결과 받기/Solution.java
@@ -1,73 +1,63 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[][] userAndReportIdArray = new int[id_list.length][id_list.length];
-        int[] stop_report_cnt = new int[id_list.length];
-        int[] stop_id = new int[id_list.length];
-
-        int id_list_length = id_list.length;
-        int id_report_length = report.length;
-
-        for(int i = 0; i < id_report_length; i++)
-        {
-            int report_null_index = report[i].indexOf(" ");
-            String user_id = report[i].substring(0, report_null_index);
-            String report_id = report[i].substring(report_null_index + 1, report[i].length());    
-
-            for(int user_point = 0; user_point < id_list_length; user_point++)
-            {
-                if(user_id.equals(id_list[user_point]))
-                {
-                    for(int report_point = 0; report_point < id_list_length; report_point++)
-                    {
-                        if(report_id.equals(id_list[report_point]))
-                        {
-                            if(user_point != report_point)
-                            {
-                                if(userAndReportIdArray[user_point][report_point] == 0)
-                                {
-                                    userAndReportIdArray[user_point][report_point]++;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-
-        for(int i = 0; i < id_list_length; i++)
-        {
-            for(int j = 0; j < id_list_length; j++)
-            {
-                if(userAndReportIdArray[i][j] != 0)
-                {
-                    stop_report_cnt[j]++;
-                }
-            }    
-        }
-
-        for(int i = 0; i < id_list_length; i++)
-        {
-            if(stop_report_cnt[i] >= k)
-            {
-                stop_id[i]++;
-            }
-        }
-
         int[] answer = new int[id_list.length];
 
-        for(int i = 0; i < id_list_length; i++)
+        // 중복 제거 Set
+        report = new HashSet<>(Arrays.asList(report)).toArray(new String[0]);
+        
+        // String : 유저 id, Integer : mail 받을 횟수
+        // 저장한 순서를 유지하기 위해 LinkedHashMap 사용
+        HashMap<String, Integer> idInfo = new LinkedHashMap<>();
+
+        // String : 신고 당한 id, ArrayList<String> : 신고 한 사람 List
+		HashMap<String, ArrayList<String>> reportInfo = new HashMap<>();
+        
+        // 1. idInfo에 id list 저장
+        for (int i = 0; i < id_list.length; i++)
         {
-            for(int j = 0; j < id_list_length; j++)
+            idInfo.put(id_list[i], 0);    
+        }
+        
+        // 2. report 내용을 통해 신고 당한 id와 신고 한 사람 List를 reportInfo에 저장
+		for (int i = 0; i < report.length; i++) 
+        {
+			String[] id = report[i].split(" ");
+			String user_id = id[0];
+			String target_id = id[1];
+            
+			if (reportInfo.containsKey(target_id)) 
+            { 
+                reportInfo.get(target_id).add(user_id);
+			} 
+            else 
+            { 
+				ArrayList<String> list = new ArrayList<String>();
+				list.add(user_id);
+				reportInfo.put(target_id, list);
+			}
+		}
+        
+        // 3. reportInfo key 값(신고 당한 id)에 size와 신고 당한 횟수를 비교해서 충족 여부 확인 및 idInfo에 메일 전달 횟수 업데이트
+        for (String report_id : reportInfo.keySet())
+        {
+            // 신고 당한 id의 신고 한 id list 크기는 신고 당한 횟수와 같음
+            if(reportInfo.get(report_id).size() >= k)
             {
-                if(userAndReportIdArray[i][j] != 0 && stop_id[j] != 0)
+                // idInfo에 신고 당한 횟수에 만족한 id를 신고한 id list의 메일 전달 횟수 업데이트
+                for (String i : reportInfo.get(report_id)) 
                 {
-                    answer[i]++;
-                }   
+					idInfo.put(i, idInfo.getOrDefault(i, 0) + 1);
+				}
             }
         }
-
+        
+        // 4. return 메일 전달 횟수
+        for(int i = 0; i < id_list.length; i++)
+        {
+            answer[i] = idInfo.get(id_list[i]);    
+        }
         return answer;
     }
 }
