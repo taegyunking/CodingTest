ec7bd463cd912141c318f8de1b173ff4a8018a65
tgan <atg940706@gmail.com>
2022-02-11 20:20:16 +0900

    없는 숫자 더하기 (다른 사람  풀이)
    1. 모든 숫자의 합에서 입력된 숫자들을 뺴주면 남은 숫자들의 합이 나옴

--- Level1/없는 숫자 더하기/Solution.java
+++ Level1/없는 숫자 더하기/Solution.java
@@ -1,32 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] numbers) {
-        int answer = -1;
-        Arrays.sort(numbers);
-        
-        int num_pos = 0;
-        int sum = 0;
-        for(int i = 0; i < 10; i++)
-        {
-            if(num_pos >= numbers.length)
-            {
-                sum += i;
-            }
-            else
-            {
-                if(i != numbers[num_pos])
-                {
-                    sum += i;
-                }
-                else
-                {
-                    num_pos++;
-                }
-            }
+        int sum = 45;
+        for (int i : numbers) {
+            sum -= i;
         }
-        
-        answer = sum;
-        return answer;
+        return sum;
     }
 }
