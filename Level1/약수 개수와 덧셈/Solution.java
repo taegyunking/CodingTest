753a170999f7cb04ec2d95609de78123c978c124
tgan <atg940706@gmail.com>
2022-02-15 11:32:22 +0900

    약수 개수와 덧셈 재풀이
    수가 제곱수의 경우 약수의 개수가 홀수이고, 아닌 경우는 짝수이다.
    제곱수인지 판별하기 위해 sqrt 함수를 이용
    수 % 제곱근 == 0이면 약수의 개수가 홀수, 아니면 약수의 개수가 짝수이다.
    ex) 16(수)의 제곱근은 8 → 수 % 제곱근 = 0 → 약수 {1,2,4,8,16} 5개 (홀수)

--- Level1/약수 개수와 덧셈/Solution.java
+++ Level1/약수 개수와 덧셈/Solution.java
@@ -4,16 +4,7 @@ class Solution {
 
         for(int i = left; i <= right; i++)
         {
-            int cnt = 0;
-            for(int j = 1; j <= i; j++)
-            {
-                if(i % j == 0)
-                {
-                    cnt++;
-                }
-            }
-
-            answer += (cnt % 2 == 0) ? i : -i;
+            answer += (i % Math.sqrt(i) == 0) ? -i : i;
         }
         return answer;
     }
