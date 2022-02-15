753bc2f58e7e0258ef0e3283206a3ca284b776dd
tgan <atg940706@gmail.com>
2022-02-15 18:07:38 +0900

    3진법 뒤집기 다른사람의 풀이
    n % 3 연산을 해서 현재 수에서 3으로 나눴을 때 나머지를 StringBuilder에 넣어서 3진법을 만든다.
    
    매번 n /= 3을 통해 n 값을 줄여나간다.
    
    StringBuilder를 사용함. String을 써서 ‘+’ 연산을 하면 매번 객체를 생성하고 더하기 때문에 속도가 느리다.
    
    **return을 할 때 Integer.parseInt를 써서 뒤에 진법을 넣어주면 해당 string을 Integer 3진법으로 바꿔서 return 된다. parseInt를 진법에 맞춰서 출력되는 것을 기억하도록 하자**

--- Level1/3진법 뒤집기/Solution.java
+++ Level1/3진법 뒤집기/Solution.java
@@ -1,31 +1,12 @@
 class Solution {
-    public int solution(int n) {        
-        int answer = 0;
-
-        int cnt = 0;
-        int num = 0;
-        while(n > num)
-        {
-            num += (2 * (int)Math.pow(3, cnt++));
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        while(n > 0){
+            sb.append(n%3);
+            n /= 3;
         }
-
-        for(int i = cnt-1; i >= 0; i--)
-        {
-            int val = 0;
-            for(int j = 2; j > 0; j--)
-            {
-                int cmp = j * (int)Math.pow(3, i);
-                if(n >= cmp)
-                {
-                    n -= cmp;
-                    val = j;
-                    break;
-                }
-            }
-
-            answer += val * (int)Math.pow(3, cnt-1-i);
-        }
-
-        return answer;
+        
+        return Integer.parseInt(sb.toString(),3);
     }
 }
